c3cc81839cafe51ea361f29a37ba4dbd911cd88f
Ynda Jas <yndajas@gmail.com>
2020-07-10 00:13:09 +0000

    Done.

--- nested.rb
+++ nested.rb
@@ -1,3 +1,4 @@
+require 'pry'
 
 def hopper
 	programmer_hash = 
@@ -16,7 +17,7 @@ def hopper
         }
      }
 
-
+  programmer_hash[:grace_hopper]
 end
 
 def alan_kay_is_known_for
@@ -37,6 +38,7 @@ def alan_kay_is_known_for
           :languages => ["C"]
         }
      }
+     programmer_hash[:alan_kay][:known_for]
 end
 
 def dennis_ritchies_language
@@ -55,6 +57,7 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
+     programmer_hash[:dennis_ritchie][:languages][0]
 end
 
 def adding_matz
@@ -80,7 +83,9 @@ def adding_matz
         }
      }
 
-    
+  programmer_hash[:yukihiro_matsumoto] = {known_for: "Ruby", languages: ["LISP", "C"]}
+  
+  programmer_hash
 end
 
 def changing_alan
@@ -102,7 +107,9 @@ def changing_alan
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
      alans_new_info = "GUI"
      
+     programmer_hash[:alan_kay][:known_for] = alans_new_info
      
+     programmer_hash
 end
 
 def adding_to_dennis
@@ -121,5 +128,9 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
+  
+  programmer_hash[:dennis_ritchie][:languages] << "Assembly"
+  
+  programmer_hash
 
 end
